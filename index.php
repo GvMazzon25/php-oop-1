04aadf3d337c0f690d2a54136dc11f6526719191
GvMazzon25 <gvmazzon25@gmail.com>
2021-12-24 17:20:33 +0100

    methods-&-cars

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<?php 
-
-class Car {
-    public $name;
-    public $prize;
-    public $incentivo;
-    public $isElectric;
-
-    function __construct($name, $prize, $incentivo = 0, $isElectric = false){
-        $this->name = $name;
-        $this->prize = $prize;
-        $this->incentivo = $incentivo;
-        $this->isElectric = $isElectric;
-    }
-} 
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,41 @@
+<?php 
+
+class Car {
+    public $name;
+    public $prize;
+    public $incentivo;
+    public $isElectric;
+    //public $isElectric;
+
+    function __construct($name, $prize, $incentivo = 0, $isElectric = false){
+        $this->name = $name;
+        $this->prize = $prize;
+        $this->incentivo = $incentivo;
+        //$this->isElectric = $isElectric;
+    }
+
+    public function getIncentivo($isElectric) {
+        if(isElectric == true) {
+           $result = substr($this->prize - $this->incentivo);
+           return $result
+        }
+    }
+} 
+
+//Car 1
+$car = new Car('fiat 500', '10.000$', '0', 'false');
+var_dump($car);
+
+echo '<p>' .$car->getIncentivo() . '..</p>';
+
+echo '<hr>';
+
+//Car 2
+$car = new Car('Tesla model S', '105.970', '30.000', 'true');
+var_dump($car);
+
+echo '<p>' .$car->getIncentivo() . '..</p>';
+
+echo '<hr>';
+
+?>
