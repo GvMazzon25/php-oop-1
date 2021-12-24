1469f4167d0c556e28af8c8c16c182e6d6e0671b
GvMazzon25 <gvmazzon25@gmail.com>
2021-12-24 16:45:58 +0100

    construct

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php 
 
+class Car {
+    public $name;
+    public $prize;
+    public $incentivo;
+    public $isElectric;
+
+    function __construct($name, $prize, $incentivo = 0, $isElectric = false){
+        $this->name = $name;
+        $this->prize = $prize;
+        $this->incentivo = $incentivo;
+        $this->isElectric = $isElectric;
+    }
+} 
+
 ?>
