1ab0bfcd47b56fa427fcc4c10210453aed7ebcca
GvMazzon25 <gvmazzon25@gmail.com>
2022-01-09 18:24:36 +0100

    prova2

--- script.php
+++ script.php
@@ -1,21 +1,39 @@
 <?php 
-class Number {
-    public $x;
-    public $y;
+class User{
+    public $name;
+    public $sold = 0;
 
-    public function getSomma() {
-        $this-> $x += $y;
+    public function setSold($eta){
+        if($eta > 65) {
+            $this->sconto = 40;
+        }
+    }
+
+    public function getSold(){
+        return $this->sconto;
     }
 }
 
-$first = new Number();
-$first-> $x = 3;
-$first-> $y = 4;
-$get_somma_first = $first->getSomma(); 
+$giorgio = new User();
+$giorgio->setSold(40);
+$sconto_giorgio = $giorgio->getSold();
 
-$second = new Number();
-$second-> $x = 3;
-$second-> $y = 4;
-$get_somma_second = $second->getSomma(); 
-
+$davide = new User();
+$davide->setSold(30);
+$sconto_davide = $giorgio->getSold();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sconto</h1>
+    <h3>Il risultato è: <?php echo "$sconto_giorgio"?></h3>
+    <h3>Il risultato è: <?php echo "$sconto_davide"?></h3>
+</body>
+</html>
